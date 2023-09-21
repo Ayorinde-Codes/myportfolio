e074cd4fff3761a901667f160c37a4f71fef024c
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2023-09-21 17:26:09 +0100

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,85 +2,85 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
 
-// module.exports = {
-//   entry: ['./src/index.js'],
-//   output: {
-//     path: path.resolve(__dirname, "build"),
-//     filename: 'bundle.js'
-//   },
-//   devtool: "source-map",
-//   plugins: [
-//     new Dotenv({
-//       systemvars: true
-//     }),
-//     new HtmlWebpackPlugin({
-//       template: path.join(__dirname, "public", "index.html"),
-//       // template: "src/index.html",
-//     })
-//   ],
-//   devServer: {
-//     port: 3030, // you can change the port
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/, // .js and .jsx files
-//         exclude: /node_modules/, // excluding the node_modules folder
-//         use: {
-//           loader: "babel-loader",
-//         },
-//       },
-//       {
-//         test: /\.(sa|sc|c)ss$/, // styles files
-//         use: ["style-loader", "css-loader"],
-//       },
-//       {
-//         test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-//         loader: "url-loader",
-//         options: { limit: false },
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: ["*", ".js", ".jsx"],
-//   },
-// };
-
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
+    path: path.resolve(__dirname, "build"),
     filename: 'bundle.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader", // for styles
-        ],
-      },
-    ],
-  },
-
+  devtool: "source-map",
   plugins: [
     new Dotenv({
       systemvars: true
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
+      // template: "src/index.html",
     })
   ],
+  devServer: {
+    port: 3030, // you can change the port
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
 };
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'build'),
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.?js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-env", "@babel/preset-react"],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           "style-loader",
+//           "css-loader", // for styles
+//         ],
+//       },
+//     ],
+//   },
+
+//   plugins: [
+//     new Dotenv({
+//       systemvars: true
+//     }),
+//     new HtmlWebpackPlugin({
+//       template: path.join(__dirname, "public", "index.html"),
+//     })
+//   ],
+// };
